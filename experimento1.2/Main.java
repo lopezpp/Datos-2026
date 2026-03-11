fa4a219db7361c41749b8f1ddaaa6c5e7cee0d91
lopezpp <cristian.lopezpa@campusucc.edu.co>
2026-03-11 19:07:56 +0000

    03

--- experimento1.2/Main.java
+++ experimento1.2/Main.java
@@ -58,6 +58,10 @@ public class Main {
                 default:
                     System.out.println("Opción no válida.");
             }
+            while (opcion != 7)
+                scanner.close();
+                
+            
         }
     }
 
